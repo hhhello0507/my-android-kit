bbc4a92d34b767e291983fa918abf1cf00403334
bestswlkh0310 <hhhello0507@dgsw.hs.kr>
2024-06-02 18:36:27 +0900

    fix : maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.android.library") version "8.2.2" apply false
     id("com.android.application") version "8.2.2" apply false
-}
-
-buildscript {
-    dependencies {
-        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
-    }
+    id("com.github.dcendents.android-maven") version "2.1" apply false
 }
